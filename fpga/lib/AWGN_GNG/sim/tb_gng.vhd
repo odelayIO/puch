b261a9830f5280480deada5020d78ee403008564
Everett <everett@odelay.io>
2025-02-06 03:53:39 -0800

    updated testbench for AWGN

--- fpga/lib/AWGN_GNG/sim/tb_gng.vhd
+++ fpga/lib/AWGN_GNG/sim/tb_gng.vhd
@@ -1,35 +1,41 @@
--- ------------------------------------------------------------------------------
--- tb_gng.vhdl
--- This file is part of the Gaussian Noise Generator IP Core
---
--- Description
---     VHDL testbench for module gng. Generate noise sequences of
---     length N and output to file.
---
--- ------------------------------------------------------------------------------
---
--- Copyright (C) 2014, Guangxi Liu <guangxi.liu@opencores.org>
---
--- This source file may be used and distributed without restriction provided
--- that this copyright statement is not removed from the file and that any
--- derivative work contains the original copyright notice and the associated
--- disclaimer.
---
--- This source is free software; you can redistribute it and/or modify it
--- under the terms of the GNU Lesser General Public License as published by
--- the Free Software Foundation; either version 2.1 of the License,
--- or (at your option) any later version.
---
--- This source is distributed in the hope that it will be useful, but
--- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
--- or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
--- License for more details.
---
--- You should have received a copy of the GNU Lesser General Public License
--- along with this source; if not, download it from
--- http://www.opencores.org/lgpl.shtml
---
--- ------------------------------------------------------------------------------
+--#############################################################################################
+--#############################################################################################
+--#
+--#   The MIT License (MIT)
+--#   
+--#   Copyright (c) 2023 http://odelay.io 
+--#   
+--#   Permission is hereby granted, free of charge, to any person obtaining a copy
+--#   of this software and associated documentation files (the "Software"), to deal
+--#   in the Software without restriction, including without limitation the rights
+--#   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--#   copies of the Software, and to permit persons to whom the Software is
+--#   furnished to do so, subject to the following conditions:
+--#   
+--#   The above copyright notice and this permission notice shall be included in all
+--#   copies or substantial portions of the Software.
+--#   
+--#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--#   SOFTWARE.
+--#   
+--#   Contact : <everett@odelay.io>
+--#  
+--#   Description : Xilinx Vitis HLS Fixed Gain Block for AXI Stream Interface
+--#
+--#   Version History:
+--#   
+--#       Date        Description
+--#     -----------   -----------------------------------------------------------------------
+--#      2024-02-04    Original Creation
+--#
+--###########################################################################################
+--###########################################################################################
+
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -43,41 +49,28 @@ end tb_gng;
 architecture Behavioral of tb_gng is
 
     -- Constants
-    constant ClkPeriod : time := 10.0 ns;
-    constant Dly       : time := 1.0 ns;
-    constant N         : integer := 1000;
+    constant ClkPeriod  : time := 10.0 ns;
+    constant N          : integer := 1000;
+    constant F_FRAC     : integer := 11; -- fractional bits output of GNG
+
+    file fpOut          : text open write_mode is
+      "../../../../../../../lib/AWGN_GNG/sim/gng_data_out.txt";
 
     -- Signals
-    signal clk        : std_logic := '0';
-    signal rstn       : std_logic := '0';
-    signal ce         : std_logic := '0';
+    signal clk          : std_logic := '0';
+    signal rstn         : std_logic := '0';
+    signal ce           : std_logic := '0';
     signal valid_out_I  : std_logic := '0';
     signal data_out_I   : std_logic_vector(15 downto 0);
     signal valid_out_Q  : std_logic := '0';
     signal data_out_Q   : std_logic_vector(15 downto 0);
 
-    file fpOut        : text;
-
-
-    -- Instantiate the gng module for I and Q with different INIT_Z values
-    component gng is
-      generic (
-        INIT_Z1     : std_logic_vector(63 downto 0) := x"6094bc1bd3d8db9a";
-        INIT_Z2     : std_logic_vector(63 downto 0) := x"d36035b0ca17e666";
-        INIT_Z3     : std_logic_vector(63 downto 0) := x"e2d0b140ab8ac10b"
-      );
-      port (
-        clk         : in std_logic;
-        rstn        : in std_logic;
-        ce          : in std_logic;
-        valid_out   : out std_logic;
-        data_out    : out std_logic_vector(15 downto 0)
-      );
-    end component;
 
 begin
 
+    -- ----------------------------------------
     -- Clock Generation Process
+    -- ----------------------------------------
     clk_gen: process
     begin
         clk <= '0';
@@ -86,7 +79,9 @@ begin
         wait for ClkPeriod / 2;
     end process clk_gen;
 
+    -- ----------------------------------------
     -- Reset Process
+    -- ----------------------------------------
     rstn_gen: process
     begin
         rstn <= '0';
@@ -95,62 +90,65 @@ begin
         wait;
     end process rstn_gen;
 
-    -- Enable Signal Process
+
+    -- ----------------------------------------
+    -- Test the CE
+    -- ----------------------------------------
     ce_gen: process
     begin
+
+        -- Assert ce initially
         ce <= '1';
-        wait;
-    end process ce_gen;
+        wait for 1 us;
 
-    -- Main Test Process
-    process
-    begin
-        -- Open file for writing
-        file_open(fpOut, "gng_data_out.txt", write_mode);
 
-        -- Deassert ce initially
-        --ce <= '0';
-
-        -- Wait some time and then activate ce for noise generation
-        wait for ClkPeriod * 10;
-        for i in 1 to N loop
-            wait until rising_edge(clk);
-            wait for Dly;
-            --ce <= '1';
-        end loop;
-
-        -- Deassert ce after noise generation
+        -- Deassert ce 
         wait until rising_edge(clk);
-        wait for Dly;
-        --ce <= '0';
+        ce <= '0';
+        wait for 3 us;
 
-        -- Close the file after some time
-        wait for ClkPeriod * 8192;
-        file_close(fpOut);
+        -- Assert ce 
+        wait until rising_edge(clk);
+        ce <= '1';
+        wait for 6 us;
+
+        -- Deassert ce 
+        wait until rising_edge(clk);
+        ce <= '0';
+        wait for 4 us;
+
+        -- Assert ce 
+        wait until rising_edge(clk);
+        ce <= '1';
 
         -- Stop simulation
         assert false report "Simulation stopped" severity note;
         wait;
-    end process;
+    end process ce_gen;
 
+
+    -- ----------------------------------------
     -- Record data
-    process(clk)
+    -- ----------------------------------------
+    write_file: process(clk)
       variable v_line   : line;
     begin
         if rising_edge(clk) then
             if valid_out_I = '1' then
                 -- Write data to file
-                write(v_line, to_integer(signed(data_out_I)));
+                write(v_line, real(to_integer(signed(data_out_I)))/2.0**F_FRAC);
                 --write(v_line, ',');
                 write(v_line, string'(", "));
-                write(v_line, to_integer(signed(data_out_Q)));
+                write(v_line, real(to_integer(signed(data_out_Q)))/2.0**F_FRAC);
                 writeline(fpOut, v_line);
             end if;
         end if;
-    end process;
+    end process write_file;
 
+    -- --------------------------------------------------------
     -- Instantiate U_gng_I (example values for INIT_Z)
-    u_gng_I : gng
+    -- --------------------------------------------------------
+    u_gng_I : entity work.gng
       generic map (
         INIT_Z1     => x"6094bc1bd3d8db9a",
         INIT_Z2     => x"d36035b0ca17e666",
@@ -164,8 +162,10 @@ begin
         data_out    => data_out_I
       );
 
+    -- --------------------------------------------------------
     -- Instantiate U_gng_Q (example values for INIT_Z)
-    u_gng_Q : gng
+    -- --------------------------------------------------------
+    u_gng_Q : entity work.gng
       generic map (
         INIT_Z1     => x"bc6840786a43d19a",
         INIT_Z2     => x"4756b3c2f33b8c9f",
--- fpga/lib/timestamp/Timestamp_Pkg.vhd
+++ fpga/lib/timestamp/Timestamp_Pkg.vhd
@@ -6,7 +6,7 @@
 --****
 --****
 --****
---****    Auto Generated on Jan. 30, 2025 12:54:42 PM
+--****    Auto Generated on Feb. 06, 2025 01:40:22 AM
 --****
 --***********************************************************************************************************
 --***********************************************************************************************************
@@ -27,11 +27,11 @@ package Timestamp_Pkg is
   -- -------------------------------------------------------------------------------------------
   constant TS_YEAR_1    : std_logic_vector(7 downto 0) := x"20";
   constant TS_YEAR_0    : std_logic_vector(7 downto 0) := x"25";
-  constant TS_MONTH     : std_logic_vector(7 downto 0) := x"01";
-  constant TS_DAY       : std_logic_vector(7 downto 0) := x"30";
-  constant TS_HOUR      : std_logic_vector(7 downto 0) := x"12";
-  constant TS_MIN       : std_logic_vector(7 downto 0) := x"54";
-  constant TS_SEC       : std_logic_vector(7 downto 0) := x"42";
+  constant TS_MONTH     : std_logic_vector(7 downto 0) := x"02";
+  constant TS_DAY       : std_logic_vector(7 downto 0) := x"06";
+  constant TS_HOUR      : std_logic_vector(7 downto 0) := x"01";
+  constant TS_MIN       : std_logic_vector(7 downto 0) := x"40";
+  constant TS_SEC       : std_logic_vector(7 downto 0) := x"22";
  
  
  
