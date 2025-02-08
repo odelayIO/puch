6de123ef4d05ef8370b53f655f809f9b3de59292
odelayIO <support@odelay.io>
2025-02-07 16:50:17 -0800

    updated source name

--- fpga/lib/AWGN_GNG/sources.tcl
+++ fpga/lib/AWGN_GNG/sources.tcl
@@ -7,7 +7,7 @@ add_files -norecurse ${DOCKER_BASE}/fpga/lib/AWGN_GNG/gng_smul_16_18.v
 add_files -norecurse ${DOCKER_BASE}/fpga/lib/AWGN_GNG/gng.v
 add_files -norecurse ${DOCKER_BASE}/fpga/lib/AWGN_GNG/hw/awgn_pkg.vhd
 add_files -norecurse ${DOCKER_BASE}/fpga/lib/AWGN_GNG/hw/awgn_reg.vhd
-add_files -norecurse ${DOCKER_BASE}/fpga/lib/AWGN_GNG/hw/awgn_top.vhd
+add_files -norecurse ${DOCKER_BASE}/fpga/lib/AWGN_GNG/gng_top.vhd
 add_files -fileset sim_1 -norecurse ${DOCKER_BASE}/fpga/lib/AWGN_GNG/sim/tb_gng.vhd
 
 set_property file_type {VHDL 2008} [get_files  ${DOCKER_BASE}/fpga/lib/AWGN_GNG/hw/awgn_reg.vhd]
