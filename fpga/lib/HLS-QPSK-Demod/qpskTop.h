379bfc3173cc7ecdecda66b7728ecf9a3c40da57
odelayIO <odelayIO@odelay.io>
2025-02-13 22:11:05 -0800

    adding HLS QPSK source code

---
+++ fpga/lib/HLS-QPSK-Demod/qpskTop.h
@@ -0,0 +1,10 @@
+#ifndef QPSKTOP_H
+#define QPSKTOP_H
+
+#include "qpsk.h"
+
+//bool qpskElementDemodulatorTimingPhase(double sampleIn, Symbol *out);
+bool qpskElementDemodulatorTimingPhase(din_t fp_din, Symbol *out);
+
+#endif
+//
