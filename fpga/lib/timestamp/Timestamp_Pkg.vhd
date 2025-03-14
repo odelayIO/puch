c275a7aa12430a1cc7e6b770d029ffcaf8b04cd9
Everett <everett@odelay.io>
2025-03-13 22:46:22 -0700

    updated ZMQ GRC and verified with PYNQ Notebook

--- fpga/lib/timestamp/Timestamp_Pkg.vhd
+++ fpga/lib/timestamp/Timestamp_Pkg.vhd
@@ -6,7 +6,7 @@
 --****
 --****
 --****
---****    Auto Generated on Mar. 13, 2025 05:25:34 PM
+--****    Auto Generated on Mar. 13, 2025 10:43:54 PM
 --****
 --***********************************************************************************************************
 --***********************************************************************************************************
@@ -29,9 +29,9 @@ package Timestamp_Pkg is
   constant TS_YEAR_0    : std_logic_vector(7 downto 0) := x"25";
   constant TS_MONTH     : std_logic_vector(7 downto 0) := x"03";
   constant TS_DAY       : std_logic_vector(7 downto 0) := x"13";
-  constant TS_HOUR      : std_logic_vector(7 downto 0) := x"17";
-  constant TS_MIN       : std_logic_vector(7 downto 0) := x"25";
-  constant TS_SEC       : std_logic_vector(7 downto 0) := x"34";
+  constant TS_HOUR      : std_logic_vector(7 downto 0) := x"22";
+  constant TS_MIN       : std_logic_vector(7 downto 0) := x"43";
+  constant TS_SEC       : std_logic_vector(7 downto 0) := x"54";
  
  
  
