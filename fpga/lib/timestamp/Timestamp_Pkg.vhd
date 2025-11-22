0d624237b5db43b373869b42c24fd2f4cc7c4e84
OdelayIO <everett@odelay.io>
2025-11-22 08:10:19 -0800

    updated the jupyter notebooks with Puch Host Interface

--- fpga/lib/timestamp/Timestamp_Pkg.vhd
+++ fpga/lib/timestamp/Timestamp_Pkg.vhd
@@ -6,7 +6,7 @@
 --****
 --****
 --****
---****    Auto Generated on Nov. 13, 2025 11:42:10 PM
+--****    Auto Generated on Nov. 15, 2025 08:27:45 AM
 --****
 --***********************************************************************************************************
 --***********************************************************************************************************
@@ -28,10 +28,10 @@ package Timestamp_Pkg is
   constant TS_YEAR_1    : std_logic_vector(7 downto 0) := x"20";
   constant TS_YEAR_0    : std_logic_vector(7 downto 0) := x"25";
   constant TS_MONTH     : std_logic_vector(7 downto 0) := x"11";
-  constant TS_DAY       : std_logic_vector(7 downto 0) := x"13";
-  constant TS_HOUR      : std_logic_vector(7 downto 0) := x"23";
-  constant TS_MIN       : std_logic_vector(7 downto 0) := x"42";
-  constant TS_SEC       : std_logic_vector(7 downto 0) := x"10";
+  constant TS_DAY       : std_logic_vector(7 downto 0) := x"15";
+  constant TS_HOUR      : std_logic_vector(7 downto 0) := x"08";
+  constant TS_MIN       : std_logic_vector(7 downto 0) := x"27";
+  constant TS_SEC       : std_logic_vector(7 downto 0) := x"45";
  
  
  
